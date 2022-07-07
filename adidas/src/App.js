0265db8f6c286a621f387763e164570e9a6fcdc8
Sofia Genchi <sofigen16@gmail.com>
2022-07-06 23:22:07 -0300

    adding title

--- adidas/src/App.js
+++ adidas/src/App.js
@@ -5,17 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Adidas Store</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Sofia Genchi
         </a>
       </header>
     </div>
