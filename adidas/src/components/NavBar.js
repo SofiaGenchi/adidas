b5d95ab50cbcbe218c4c26a142b04dd63ca5b391
Sofia Genchi <sofigen16@gmail.com>
2022-07-13 18:47:00 -0300

    fixes in the nav

--- adidas/src/components/NavBar.js
+++ adidas/src/components/NavBar.js
@@ -1,59 +1,29 @@
 import '../scss/NavBar.scss'
 
-// function NavBar() {
-//     return (
-//         <ul>
-//             <li>Home</li>
-//             <li>Home</li>
-//             <li>Home</li>
-//             <li>Home</li>
-//             <li>Home</li>
-//             <li>Home</li>
-//             <li>Home</li>
-//             <li>Home</li>
-//         </ul>
-//     );
-//   }
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#"><img src='./adidas.png'></img></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <ul>
-                <li>Hombre</li>
-                <li>Mujer</li>
-                <li>Niños</li>
-            </ul>
+    <Navbar collapseOnSelect expand="lg" bg="" variant="light">
+      <Container>
+        <Navbar.Brand href="#home"><img className='logo' src='/adidas.png'></img></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Hombre</Nav.Link>
+            <Nav.Link href="#pricing">Mujer</Nav.Link>
+            <Nav.Link href="#pricing">Niños</Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Buscar"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Buscar</Button>
-          </Form>
+          <Nav>
+              <ion-icon name="bag-outline"></ion-icon>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
-
-// export default NavScrollExample;
 
 export default NavBar;
