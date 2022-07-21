a4137f7e0e48a40db231252d700b57ac6efc4303
Sofia Genchi <sofigen16@gmail.com>
2022-07-20 22:11:58 -0300

    arreglo en el contador

--- adidas/src/components/ItemCount.js
+++ adidas/src/components/ItemCount.js
@@ -4,19 +4,19 @@ import Card from 'react-bootstrap/Card';
 import '../scss/_ItemCount.scss'
 
 function ItemCount() {
-const[number, setNumber] = useState("");
+const[producto, setNumber] = useState(0);
   const agregar = () => {
-    setNumber(number + 1);
+    setNumber(producto + 1);
   }
   const eliminar = () => {
-    setNumber(number - 1)
+    setNumber(producto - 1)
   }
   return (
     <div className='count'>
         <Card style={{ width: '18rem' }}>
             <Card.Body>
                 <Card.Title>Producto Adidas</Card.Title>
-                <Card.Text><p>{number}</p></Card.Text>
+                <Card.Text><p>{producto}</p></Card.Text>
                 <Button onClick={agregar} variant="primary">Agregar</Button>
                 <Button onClick={eliminar} variant="primary">Eliminar</Button>
             </Card.Body>
