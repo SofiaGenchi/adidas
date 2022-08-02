9ce0cce6b9f10bc47866111542d0b52f0b44059a
Sofia Genchi <sofigen16@gmail.com>
2022-08-02 15:39:28 -0300

    Update Item

--- adidas/src/components/Item.js
+++ adidas/src/components/Item.js
@@ -6,17 +6,19 @@ import { Link } from "react-router-dom";
 
 const Item = ({producto}) => {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={producto.img} alt={producto.name} />
-            <Card.Body>
-                <Card.Title>{producto.name}</Card.Title>
-                <h4>$ {producto.price}</h4>
-                <p>Stock disponible: {producto.stock}</p>
-                <Link to={`/item/${producto.id}`}>
-                <Button variant="secondary">Detalles del producto</Button>
-                </Link>
-            </Card.Body>
-        </Card>
+        <Link to={`item/${producto.id}`}>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={producto.img} alt={producto.name} />
+                <Card.Body>
+                    <Card.Title>{producto.name}</Card.Title>
+                    <h4>$ {producto.price}</h4>
+                    <p>Stock disponible: {producto.stock}</p>
+                    <Link to={`/item/${producto.id}`}>
+                    <Button variant="secondary">Detalles del producto</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+        </Link>
     )
 }
 
