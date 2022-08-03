77e4a7907a5f939435868fe6bd1819f3f47a98da
Sofia Genchi <sofigen16@gmail.com>
2022-08-03 11:01:26 -0300

    Update FakeApi

--- adidas/src/mock/FakeApi.js
+++ adidas/src/mock/FakeApi.js
@@ -4,7 +4,8 @@ export const productos = [
         description: 'Esta camiseta titular de Boca Juniors adidas irradia la energía de principios de los 90 con su cuello distintivo y la franja cosida en el pecho. En contraste con sus detalles retro, los modernos paneles de mesh y la tecnología de absorción AEROREADY se combinan para mantener cómodos a los hinchas del club. El escudo tejido es el toque final que completa este estilo de fútbol.',
         price: 14.999,
         img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/4fcfb797d457430eaccaaec200dafeb7_9366/Camiseta_Titular_Boca_Juniors_22-23_Azul_HE6323_01_laydown.jpg',
-        stock: 8
+        stock: 8,
+        category: 'camiseta'
     },
     {
         id: 2,
@@ -12,7 +13,8 @@ export const productos = [
         description: 'Manteniéndose fiel al rico legado del Ajax de Amsterdam, esta camiseta titular adidas conserva un look atemporal. Una segunda mirada revela detalles de finas rayas y hebras de oro que celebran una historia de éxito resplandeciente. Diseñada para jugadores e hinchas, esta camiseta brinda comodidad en todo momento gracias a sus paneles de malla y tecnología de absorción AEROREADY.',
         price: 14.999,
         img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/b30e2d27fcac4efeb84aaea500a0829f_9366/Camiseta_Titular_Ajax_Amsterdam_22-23_Rojo_H58243_21_model.jpg',
-        stock: 4
+        stock: 4,
+        category: 'camiseta'
     },
     {
         id: 3,
@@ -20,7 +22,8 @@ export const productos = [
         description: 'Durante su historia repleta de trofeos, las estrellas se han convertido en una parte tan importante de la identidad de la Juventus como las franjas. Esta camiseta de fútbol de adidas une ambas cosas, formando las icónicas rayas negras del club a partir de los pequeños símbolos en forma de estrella. Confeccionada especialmente para hinchas del club, combina comodidad y orgullo con tecnología de absorción AEROREADY y un escudo bordado.',
         price: 14.999,
         img: 'https://assets.adidas.com/images/h_320,f_auto,q_auto,fl_lossy,c_fill,g_auto/ef37b79dac5f41da8dd8ae8a009d8814_9366/Camiseta_Uniforme_de_Titular_Juventus_22-23_Blanco_H38907_21_model.jpg',
-        stock: 24
+        stock: 24,
+        category: 'camiseta'
     },
     {
         id: 4,
@@ -28,7 +31,80 @@ export const productos = [
         description: 'Con su diseño sencillo en rojo y cuello redondo acanalado blanco, esta camiseta de fútbol de titular adidas se inspira en el uniforme icónico que los llevó a algunos de sus momentos más memorables. Diseñada para hinchas, esta camiseta brinda comodidad mientras ves a una nueva generación de jugadores perseguir la gloria en la cancha gracias a su tejido suave con tecnología de absorción AEROREADY.',
         price: 14.999,
         img: 'https://assets.adidas.com/images/h_320,f_auto,q_auto,fl_lossy,c_fill,g_auto/bfac4aab68ec4ddb82dcad2c0074a997_9366/Camiseta_Titular_Manchester_United_21-22_Rojo_H31447_21_model.jpg',
-        stock: 10
+        stock: 10,
+        category: 'camiseta'
+    },
+    {
+        id: 5,
+        name: 'BUZO CON CAPUCHA DAME 8 INNOVATION',
+        description: 'Está confeccionado en felpa francesa para una sensación suave y cómoda. El bolsillo canguro te permite guardar el teléfono, las llaves o la billetera. El logo de Dame en el pecho te permite representar a tu superhéroe favorito.',
+        price: 25.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/b503ae10553c4cf6ab92adcd017b417b_9366/Buzo_con_Capucha_Dame_8_Innovation_Negro_HE5464_25_model.jpg',
+        stock: 12,
+        category: 'buzo'
+    },
+    {
+        id: 6,
+        name: 'BUZO CON CAPUCHA ADICOLOR CLASSICS 3 TIRAS',
+        description: '¿Día ajetreado? A quién le importa. Avanzá a tu propio ritmo y disfrutá de cada momento envuelto en la comodidad de este buzo con capucha adidas. Es fácil de hacer gracias a su tejido de felpa suave. Subí la capucha, guardá tus manos en el bolsillo frontal y desconectate del ruido exterior.',
+        price: 24.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/0fad30cbc5754833ab2eadb6008492c5_9366/Buzo_con_Capucha_Adicolor_Classics_3_Tiras_Azul_HE9476_21_model.jpg',
+        stock: 32,
+        category: 'buzo'
+    },
+    {
+        id: 7,
+        name: 'BUZO CON CAPUCHA ADIDAS REKIVE',
+        description: 'Un buen buzo con capucha es una verdadera victoria. Como este creado por adidas. Es tan cómodo que podés relajarte en el sofá y se ve muy bien cuando salís a las calles. Además, tiene ribetes que se destacan, diferenciándolo de los demás buzos con capucha. Así que lucilo y disfrutá. La comodidad. El look. Lo tenés todo.',
+        price: 28.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/d2595d90c92f4d009ddcae6901122383_9366/Buzo_con_Capucha_adidas_Rekive_Rosa_HK7312_21_model.jpg',
+        stock: 40,
+        category: 'buzo'
+    },
+    {
+        id: 8,
+        name: 'BUZO CON CAPUCHA ADICOLOR ESSENTIALS TRIFOLIO',
+        description: 'Divertíte un poco con tu look diario. Este buzo con capucha adidas Adicolor luce un estampado desteñido que te permite hacer justo eso. Fácil de combinar en una silueta casual pero moderna, te hace sentir como la mejor versión de vos mismo. Su tejido de felpa gruesa brinda abrigado en los días más fríos.',
+        price: 28.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/1a5920fa70644bf295acad92012ff801_9366/Buzo_con_Capucha_Adicolor_Essentials_Trifolio_Gris_HE9436_21_model.jpg',
+        stock: 1,
+        category: 'buzo'
+    },
+    {
+        id: 9,
+        name: 'CAMPERA TEAMGEIST FC BAYERN TEJIDA',
+        description: ' Esta campera de fútbol del FC Bayern se remonta al verano de 2006 para conmemorar los 15 años de adidas Teamgeist. Su tejido suave garantiza comodidad, mientras que las aberturas para los pulgares te permiten experimentar un mayor abrigo en los días más fríos. El escudo del club bordado y el clásico logo Teamgeist se encargan de que cada detalle cuente.',
+        price: 25.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/e4f52de7392649ebaa25ad9f00d329e4_9366/Campera_Teamgeist_FC_Bayern_Tejida_Rojo_H67173_21_model.jpg',
+        stock: 9,
+        category: 'campera'
+    },
+    {
+        id: 10,
+        name: 'CAMPERA ALCOLCHADA REVERSIBLE ADIDAS ADVENTURE',
+        description: 'Salí de casa en los días fríos preparado para cualquier cosa con esta campera adidas acolchada. El aislamiento térmico retiene el calor mientras que los bolsillos ofrecen un lugar seguro para tus pertenencias más esenciales. Un lado te permite lucir un look estilizado y el otro un clásico look acolchado. La versatilidad es clave.',
+        price: 43.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/faec18930c324f70a765ad380098bf93_9366/Campera_Acolchada_Reversible_adidas_Adventure_Negro_H13573_21_model.jpg',
+        stock: 1,
+        category: 'campera'
+    },
+    {
+        id: 11,
+        name: 'CAMPERA ANTHEM TIRO BOCA JUNIORS',
+        description: 'Cuando los jugadores de Boca se ponen de pie hombro con hombro antes del silbato inicial, lucen esta campera de fútbol adidas. Su corte ceñido se ajusta al cuerpo para que el plantel pueda concentrarse en los movimientos finales del calentamiento antes de empezar el partido. Además, tiene el escudo del club tejido en el pecho. Este producto incorpora material reciclado como parte de nuestro anhelo de ponerle fin a la contaminación por plástico.',
+        price: 22.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/5f7c8fc6897b41e9b305ad1201226a17_9366/Campera_Anthem_Tiro_Boca_Juniors_Azul_GU9588_21_model.jpg',
+        stock: 4,
+        category: 'campera'
+    },
+    {
+        id: 12,
+        name: 'CAMPERA R.Y.V DE POLIESTER',
+        description: 'Esta campera deportiva de corte holgado trae una silueta vintage de regreso. Su diseño con paneles en contraste mantiene el look atlético y el logo doble del Trifolio que luce en el frente evoca el estilo clásico de adidas. Este producto está hecho con Primegreen, una serie de materiales reciclados de alto rendimiento.',
+        price: 32.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/f1110b3b198d4641bcedad5e00410769_9366/Campera_R.Y.V._de_Poliester_Granate_H11474_21_model.jpg',
+        stock: 10,
+        category: 'campera'
     }
 ]
 
