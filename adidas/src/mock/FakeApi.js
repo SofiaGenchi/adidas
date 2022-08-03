709502dd824ea401e70df368289c861ae3c19b62
Sofia Genchi <sofigen16@gmail.com>
2022-08-03 14:51:00 -0300

    Update FakeApi

--- adidas/src/mock/FakeApi.js
+++ adidas/src/mock/FakeApi.js
@@ -36,6 +36,78 @@ export const productos = [
     },
     {
         id: 5,
+        name: 'CAMISETA ARQUERO ARGENTINA 22',
+        description: '100% Argentina. Diseñada para los hinchas, ofrece comodidad en todo momento gracias a su tejido suave con tecnología de absorción AEROREADY. Un producto hecho parcialmente con contenido reciclado generado a partir de desechos de producción, tales como recortes de tela, y desechos domésticos postconsumo, para evitar un mayor impacto ambiental al producir contenido virgen.',
+        price: 14.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/a67e7cc1e7424c87be94aebc00e3ec27_9366/Camiseta_Arquero_Argentina_22_Verde_HF1482_25_model.jpg',
+        stock: 46,
+        category: 'camisetas'
+    },
+    {
+        id: 6,
+        name: 'CAMISETA TERCER UNIFORME ARSENAL 21/22',
+        description: 'Los 90 regresan como un relámpago. Esta camiseta del tercer uniforme del Arsenal reinventa lo mejor de una década memorable para el fútbol y para adidas. Diseñada para hinchas que buscan lo mejor de ambas épocas, su look llamativo inspirado en EQT está hecha con tejido suave supercómodo que incorpora tecnología de absorción AEROREADY.',
+        price: 14.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/72bc3b3577a644a89382ace100a6be5a_9366/Camiseta_Tercer_Uniforme_Arsenal_21-22_Azul_GM0213_41_detail.jpg',
+        stock: 34,
+        category: 'camisetas'
+    },
+    {
+        id: 7,
+        name: 'CAMISETA PREPARTIDO JUVENTUS',
+        description: 'Esta camiseta de fútbol adidas irradia el mismo look inspirado en la música electrónica del uniforme de suplente de la Juventus y mantiene a los jugadores cómodos antes del pitazo inicial. El tejido suave con tecnología de absorción AEROREADY es simplemente perfecto. El logo del equipo en contraste se destaca sobre el diseño audaz.',
+        price: 15.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/e21c6336f2964b289897ade3009aedc9_9366/Camiseta_Prepartido_Juventus_Negro_HB0444_25_model.jpg',
+        stock: 24,
+        category: 'camisetas'
+    },
+    {
+        id: 8,
+        name: 'CAMISETA VISITANTE JUVENTUS 21/22',
+        description: 'Cuando salen de viaje, la Juventus siempre encuentra el ritmo correcto. Con las 3 Tiras que cambian de color y detalles brillantes, esta camiseta de fútbol adidas obtiene la energía de la vibrante escena electrónica de Turín. Diseñada para envolver los hinchas en comodidad, combina tejido suave con tecnología de absorción AEROREADY. Luce un escudo tejido en el pecho.',
+        price: 14.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8b8f873fe73f4ef7a949ace8009fd83c_9366/Camiseta_Visitante_Juventus_21-22_Negro_GS1438_21_model.jpg',
+        stock: 10,
+        category: 'camisetas'
+    },
+    {
+        id: 9,
+        name: 'CAMISETA TITULAR FC BAYERN 22/23',
+        description: 'Rojo, blanco y apuntando a la victoria. Los colores clásicos del FC Bayern convergen en el frente de esta disruptiva camiseta de titular de adidas. Creada para mantener cómodos a los hinchas del club, combina paneles de malla con tecnología de absorción AEROREADY. Viene con un bordado del escudo del club y el lema "Mia san mia" para mostrar tu orgullo.',
+        price: 14.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/ac1154bd87cc4d239b5dae6901306a5f_9366/Camiseta_de_Titular_FC_Bayern_22-23_Rojo_H39900_21_model.jpg',
+        stock: 68,
+        category: 'camisetas'
+    },
+    {
+        id: 10,
+        name: 'CAMISETA VISITANTE MANCHESTER UNITED 21/22',
+        description: 'Orgullosos de sus raíces. Si mirás de cerca, verás que la "M" de la ciudad natal del Manchester United se ha entretejido en los diseños de algunos de los kits más memorables del club. Esta camiseta de fútbol adidas retoma ese hilo y luce un diseño abstracto y contemporáneo que evoca una camiseta clásica de principios de los 90. Diseñada para hinchas, esta camiseta brinda comodidad en todo momento gracias a su tejido suave con tecnología de absorción AEROREADY. El escudo tejido destaca tu amor profundo por el Manchester United.',
+        price: 14.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/53e87b2c46e74e85a2d8ad2b0068f35a_9366/Camiseta_Visitante_Manchester_United_21-22_Blanco_GM4621_21_model.jpg',
+        stock: 12,
+        category: 'camisetas'
+    },
+    {
+        id: 13,
+        name: 'CAMISETA TIRO',
+        description: 'Detrás de todo ese trabajo en equipo y esa unión, los jugadores de fútbol no necesitan mucho estímulo para expresarse en la cancha. Lo mismo pasa cuando están descansando. Parte de la colección adidas Tiro inspirada en la ropa que usan los jugadores en los momentos fuera de la cancha, esta camiseta Tiro se destaca por su diseño desteñido. Su tejido absorbente AEROREADY y los paneles en malla son tan prácticos en la playa como en la cancha.',
+        price: 10.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/779d579e5a264a5ea23aae4d00a503cf_9366/Camiseta_Tiro_Rojo_H60001_25_model.jpg',
+        stock: 4,
+        category: 'camisetas'
+    },
+    {
+        id: 14,
+        name: 'AFX X TFL JSY',
+        description: 'Afc X Tfl Jsy es un nuevo producto para Hombre de adidas.',
+        price: 15.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/d1206c1551ae47e49f60adb100d014dd_9366/AFC_x_TFL_JSY_Azul_HF4524_41_detail.jpg',
+        stock: 22,
+        category: 'camisetas'
+    },
+    {
+        id: 15,
         name: 'BUZO CON CAPUCHA DAME 8 INNOVATION',
         description: 'Está confeccionado en felpa francesa para una sensación suave y cómoda. El bolsillo canguro te permite guardar el teléfono, las llaves o la billetera. El logo de Dame en el pecho te permite representar a tu superhéroe favorito.',
         price: 25.999,
@@ -44,7 +116,7 @@ export const productos = [
         category: 'buzos'
     },
     {
-        id: 6,
+        id: 16,
         name: 'BUZO CON CAPUCHA ADICOLOR CLASSICS 3 TIRAS',
         description: '¿Día ajetreado? A quién le importa. Avanzá a tu propio ritmo y disfrutá de cada momento envuelto en la comodidad de este buzo con capucha adidas. Es fácil de hacer gracias a su tejido de felpa suave. Subí la capucha, guardá tus manos en el bolsillo frontal y desconectate del ruido exterior.',
         price: 24.999,
@@ -53,7 +125,7 @@ export const productos = [
         category: 'buzos'
     },
     {
-        id: 7,
+        id: 17,
         name: 'BUZO CON CAPUCHA ADIDAS REKIVE',
         description: 'Un buen buzo con capucha es una verdadera victoria. Como este creado por adidas. Es tan cómodo que podés relajarte en el sofá y se ve muy bien cuando salís a las calles. Además, tiene ribetes que se destacan, diferenciándolo de los demás buzos con capucha. Así que lucilo y disfrutá. La comodidad. El look. Lo tenés todo.',
         price: 28.999,
@@ -62,7 +134,7 @@ export const productos = [
         category: 'buzos'
     },
     {
-        id: 8,
+        id: 18,
         name: 'BUZO CON CAPUCHA ADICOLOR ESSENTIALS TRIFOLIO',
         description: 'Divertíte un poco con tu look diario. Este buzo con capucha adidas Adicolor luce un estampado desteñido que te permite hacer justo eso. Fácil de combinar en una silueta casual pero moderna, te hace sentir como la mejor versión de vos mismo. Su tejido de felpa gruesa brinda abrigado en los días más fríos.',
         price: 28.999,
@@ -71,7 +143,43 @@ export const productos = [
         category: 'buzos'
     },
     {
-        id: 9,
+        id: 18,
+        name: 'BUZO CON CAPUCHA ESSENTIALS 3 TIRAS',
+        description: 'Las noches frescas y las mañanas frías requieren del abrigo y la comodidad de este buzo con capucha adidas. Su diseño está inspirado en las prendas de entrenamiento clásicas del archivo de adidas. No hay nada mejor despues de entrenar que envolverte con la suavidad que ofrece el tejido de felpa francesa de este buzo con capucha.',
+        price: 17.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/3a50ef6ec9f94799bbb0ac7101215b3b_9366/Buzo_con_Capucha_Essentials_3_Tiras_Negro_GK9062_21_model.jpg',
+        stock: 8,
+        category: 'buzos'
+    },
+    {
+        id: 19,
+        name: 'BUZO CON CAPUCHA ADICOLOR CLEAN CLASSIC',
+        description: 'Este buzo con capucha adidas Adicolor Clean Classics te permite ser vos mismo sin sacrificar la comodidad. Su diseño tiene solo lo esencial, simplemente tejido sin tintes para un look limpio y neutral. El tejido de una mezcla de algodón grueso es sustancial y siempre se siente bien.',
+        price: 26.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/44299e9c15a04f63b2c2add7008106f9_9366/Buzo_con_Capucha_Adicolor_Clean_Classics_Blanco_HC1971_25_model.jpg',
+        stock: 40,
+        category: 'buzos'
+    },
+    {
+        id: 20,
+        name: 'BUZO CON CAPUCHA DE BASQUET LEGENDS',
+        description: 'Da lo mejor de vos con este buzo con capucha de básquet adidas. Ya sea que estés practicando tus tiros en una cancha cubierta o al aire libre, el tejido absorbente AEROREADY ayuda a mantener tu cuerpo fresco y seco. Abrí la solapa de cuatro botones para un poco más de ventilación. Las aberturas laterales a la altura del dobladillo te permiten moverte con total libertad.',
+        price: 17.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/61bbd22fc34442d99d1caddf00430de1_9366/Buzo_con_Capucha_de_Basquet_Legends_Negro_HF4196_25_model.jpg',
+        stock: 20,
+        category: 'buzos'
+    },
+    {
+        id: 21,
+        name: 'BUZO CON CAPUCHA CAMO',
+        description: 'Es algo que nunca desaparece. Obviamente estamos hablando del camuflaje. No depende de la temporada, la ocasión o el estilo. Es perfecto en todo momento. Lo cual es evidente en este buzo con capucha adidas. Una actualización atrevida del estampado clásico que no puede negarse. Además, el tejido de felpa suave te envuelve en comodidad y junto con los detalles acanalados, podés estar cómodo en todo momento.',
+        price: 24.999,
+        img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/580fb2f4d3bb418ab4b7ad9e0104c75c_9366/Buzo_con_Capucha_Graphic_Camo_Verde_HF4886_21_model.jpg',
+        stock: 40,
+        category: 'buzos'
+    },
+    {
+        id: 22,
         name: 'CAMPERA TEAMGEIST FC BAYERN TEJIDA',
         description: ' Esta campera de fútbol del FC Bayern se remonta al verano de 2006 para conmemorar los 15 años de adidas Teamgeist. Su tejido suave garantiza comodidad, mientras que las aberturas para los pulgares te permiten experimentar un mayor abrigo en los días más fríos. El escudo del club bordado y el clásico logo Teamgeist se encargan de que cada detalle cuente.',
         price: 25.999,
@@ -80,7 +188,7 @@ export const productos = [
         category: 'camperas'
     },
     {
-        id: 10,
+        id: 23,
         name: 'CAMPERA ALCOLCHADA REVERSIBLE ADIDAS ADVENTURE',
         description: 'Salí de casa en los días fríos preparado para cualquier cosa con esta campera adidas acolchada. El aislamiento térmico retiene el calor mientras que los bolsillos ofrecen un lugar seguro para tus pertenencias más esenciales. Un lado te permite lucir un look estilizado y el otro un clásico look acolchado. La versatilidad es clave.',
         price: 43.999,
@@ -89,7 +197,7 @@ export const productos = [
         category: 'camperas'
     },
     {
-        id: 11,
+        id: 24,
         name: 'CAMPERA ANTHEM TIRO BOCA JUNIORS',
         description: 'Cuando los jugadores de Boca se ponen de pie hombro con hombro antes del silbato inicial, lucen esta campera de fútbol adidas. Su corte ceñido se ajusta al cuerpo para que el plantel pueda concentrarse en los movimientos finales del calentamiento antes de empezar el partido. Además, tiene el escudo del club tejido en el pecho. Este producto incorpora material reciclado como parte de nuestro anhelo de ponerle fin a la contaminación por plástico.',
         price: 22.999,
@@ -98,7 +206,7 @@ export const productos = [
         category: 'camperas'
     },
     {
-        id: 12,
+        id: 25,
         name: 'CAMPERA R.Y.V DE POLIESTER',
         description: 'Esta campera deportiva de corte holgado trae una silueta vintage de regreso. Su diseño con paneles en contraste mantiene el look atlético y el logo doble del Trifolio que luce en el frente evoca el estilo clásico de adidas. Este producto está hecho con Primegreen, una serie de materiales reciclados de alto rendimiento.',
         price: 32.999,
