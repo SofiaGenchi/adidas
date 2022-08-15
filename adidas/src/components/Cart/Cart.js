5c25c7e1de98bdaed2460b48abf1e8396e367103
Sofia Genchi <sofigen16@gmail.com>
2022-08-15 17:24:14 -0300

    Update Cart

--- adidas/src/components/Cart/Cart.js
+++ adidas/src/components/Cart/Cart.js
@@ -7,6 +7,9 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
+import React, { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+
 const Cart = () => {
     const {cart, removeItem, clearCart} = useContext(CartContext);
     let total = 0;
@@ -15,6 +18,9 @@ const Cart = () => {
         total += cart[i].price;
     }
 
+
+
+    const [show, setShow] = useState(true);
     return(
         <div className='cartSection'>
             <Container>
@@ -22,7 +28,7 @@ const Cart = () => {
                 <Col xs></Col>
 
                 <Col xs={{ order: 12 }}>
-                    <Card border="warning" style={{ width: '18rem', display: cart.length > 0 && 'none' }}>
+                    {/* <Card border="warning" style={{ width: '18rem', display: cart.length > 0 && 'none' }}>
                         <Card.Header>ADIDAS</Card.Header>
                         <Card.Body>
                             <Card.Title>Tu carrito: <span>({cart.length})</span></Card.Title>
@@ -33,7 +39,27 @@ const Cart = () => {
                                 </Link>
                             </div>
                         </Card.Body>
-                    </Card>
+                    </Card> */}
+                    <Alert show={show} variant="success" style={{display: cart.length > 0 && 'none'}}>
+                                    <Alert.Heading>El carrito esta vacio!</Alert.Heading>
+                                    <p>
+                                    Todavia no has seleccionado ninguna prenda.
+                                    </p>
+                                    <hr />
+                                    <div className="d-flex justify-content-end">
+                                        <Link to={`/`} className="co-clear">
+                                        <Button onClick={() => setShow(false)} variant="outline-secondary">
+                                    Continuar comprando
+                                    </Button>
+                                        </Link>
+                                    
+                                    </div>
+                                    <Link to={`/`} className="co-clear">
+                                        {!show && <Button onClick={() => setShow(true)}>Continuar comprando</Button>}
+                                    </Link>
+                                </Alert>
+
+                                
 
                     <Card border="dark" style={{ width: '18rem', display: cart.length < 1 && 'none' }}>
                         <Card.Header>
